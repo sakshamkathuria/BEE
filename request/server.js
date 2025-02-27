bd19fa5a63c602d1ebbb383afa9ab15dc15edfc9
sakshamkathuria <sakshamkathuria50@gmail.com>
2025-02-27 16:20:10 +0530

    Third Commit

--- request/server.js
+++ request/server.js
@@ -1,9 +1,23 @@
 const express=require("express");
 const app=express();
+let userData = [];
+app.use(express.urlencoded({ extended: true }));
 app.get("/adduser",(req,res)=>{
-    res.sendFile("./register.html");
+    let path=__dirname+"/register.html";
+    res.sendFile(path);
 })
-app.use(express.urlencoded("true"));
+app.post("/adduser",(req,res)=>{
+    let{username,useremail,userpassword}=req.body;
+    console.log(username,useremail,userpassword);
+    let newUser={
+        name:username,
+        email:useremail,
+        password:userpassword
+    }
+    userData.push(newUser);
+    res.send(userData);
+})
 app.listen(4545,()=>{
     console.log("server started");
 })
+//express.urlencoded middleware or function passes the url encoded data to object
